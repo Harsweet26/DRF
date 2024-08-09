6fd5969be6aab5ca7404d4911063124457cc1f68
Harsweet26 <156780535+Harsweet26@users.noreply.github.com>
2024-08-09 14:01:19 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,24 +1,13 @@
-from django.utils.version import get_version
+from typing import List, Optional
 
-VERSION = (4, 2, 13, "final", 0)
-
-__version__ = get_version(VERSION)
+__version__ = "23.3.2"
 
 
-def setup(set_prefix=True):
+def main(args: Optional[List[str]] = None) -> int:
+    """This is an internal API only meant for use by pip's own console scripts.
+
+    For additional details, see https://github.com/pypa/pip/issues/7498.
     """
-    Configure the settings (this happens as a side effect of accessing the
-    first setting), configure logging and populate the app registry.
-    Set the thread-local urlresolvers script prefix if `set_prefix` is True.
-    """
-    from django.apps import apps
-    from django.conf import settings
-    from django.urls import set_script_prefix
-    from django.utils.log import configure_logging
+    from pip._internal.utils.entrypoints import _wrapper
 
-    configure_logging(settings.LOGGING_CONFIG, settings.LOGGING)
-    if set_prefix:
-        set_script_prefix(
-            "/" if settings.FORCE_SCRIPT_NAME is None else settings.FORCE_SCRIPT_NAME
-        )
-    apps.populate(settings.INSTALLED_APPS)
+    return _wrapper(args)
